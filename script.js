6260e13be95c9195abf995ac9e99069aeddc75c9
Pretinho66 <joao.lauredo@escola.pr.gov.br>
2025-07-04 11:38:37 +0000

    v6

--- script.js
+++ script.js
@@ -7,5 +7,37 @@ function mostraPalavrasChave() {
 
   const campoResultado = document.querySelector("#resultado-palavrachave");
 
-  campoResultado.textContent = texto;
+  const palavrasChave = processaTexto(texto);
+
+  campoResultado.textContent = palavrasChave.join(", ");
+}
+function processaTexto(texto) {
+  let palavras = texto.split(/\P{L}+/u);
+
+  const frequencias = contaFrequencias(palavras);
+
+  let ordenadas = Object.keys(frequencias).sort(ordenaPalavra);
+
+  function ordenaPalavra(p1, p2) {
+    return frequencias[p2] - frequencias[p1];
+  }
+
+  console.log(ordenadas);
+
+  return palavras;
+}
+function contaFrequencias(palavras) {
+  let frequencias = {};
+
+  for (let i of palavras) {
+    frequencias[i] = 0;
+
+    for (let j of palavras) {
+      if (i == j) {
+        frequencias[i]++;
+      }
+    }
+  }
+
+  return frequencias;
 }
